3179e728c2a95cc5ce828e605c5629461b00b603
Tragislav Work PC <s.dykomenko@gmail.com>
2021-09-16 10:48:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+    });
+});
